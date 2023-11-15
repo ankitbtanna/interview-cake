800e7fcfcd047df7056db5c2a468e7223efa1c87
Ankit Tanna <ankit.tanna@autogeneral.com.au>
2023-11-15 14:31:44 +1000

    add comments

--- 22-selection-sort/index.js
+++ 22-selection-sort/index.js
@@ -1,26 +1,29 @@
 function selectionSort(items) {
-    let length = items.length
-    let pass = 0
+    let length = items.length; // know the length of the array
+    let pass = 0; // we'll make multiple passes to the array, each time decrementing by one. This variable is for keeping track of passes
 
     while (pass < length) {
-        let largest = items[0] // Reset largest for each pass
-        let largestIndex = 0
+        // iterate passes
+        let largest = items[0]; // Reset largest for each pass
+        let largestIndex = 0; // Reset largestIndex for each pass
 
+        // pass until the last but passed elements
         for (let i = 1; i < length - pass; i++) {
             if (items[i] > largest) {
-                largest = items[i]
-                largestIndex = i
+                largest = items[i]; // update largest
+                largestIndex = i; // update largestIndex
             }
         }
 
-        let temp = items[length - 1 - pass]
-        items[length - 1 - pass] = largest
-        items[largestIndex] = temp
+        // swap the last element of the pass with largest element
+        let temp = items[length - 1 - pass];
+        items[length - 1 - pass] = largest;
+        items[largestIndex] = temp;
 
-        pass++
+        pass++;
     }
 
-    return items
+    return items;
 }
 
 // generate a random array of 30 numbers between 1 and 100
@@ -29,6 +32,6 @@ function selectionSort(items) {
 const items = [
     89, 31, 87, 26, 4, 9, 34, 88, 55, 71, 26, 43, 4, 24, 42, 7, 53, 18, 94, 15,
     74, 83, 94, 13, 76, 58, 94, 75, 86, 68,
-]
+];
 
-console.log(selectionSort(items))
+console.log(selectionSort(items));
